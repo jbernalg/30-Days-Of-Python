8badf1d12957e08bbca9c75b75c3c83c089bbfa8
jbernalg <jeinffersonbernal@gmail.com>
2022-05-27 11:15:54 -0500

    exercise day_2

---
+++ day_2/variables.py
@@ -0,0 +1,30 @@
+# Dia 2. 30 dia  de programacion en python
+
+name = 'Jeinfferson'
+last_name = 'Bernal'
+name_complet = 'Jose Gimenez'
+country = 'Mexico'
+age = 29
+year = 2022
+is_married = False
+is_True = True
+is_light_on = False
+shoes, computer, color = 3, 'acer', 'blue'
+
+print(type(name))
+print(type(last_name))
+print(type(name_complet))
+print(type(country))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_True))
+print(type(is_light_on))
+print(type(shoes), type(computer), type(color))
+
+#length name
+print('Length name: ', len(name))
+
+#Compare the length of your first and last name
+print(f'''Length name: {len(name)}
+Length last name: {len(last_name)}''')
