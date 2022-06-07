af06f2feee8a6513fc8283dc61cc21470f972fd7
jbernalg <jeinffersonbernal@gmail.com>
2022-06-07 10:23:19 -0500

    exercise4

---
+++ day_4/exercise4.py
@@ -0,0 +1,20 @@
+#Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'
+print('Thirty ' + 'Days ' + 'Of ' + 'Python') 
+
+#Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'
+string1 = 'Coding '
+string2 = 'For '
+string3 = 'All'
+complet = string1 + string2 + string3
+print(complet)
+
+#Declare a variable named company and assign it to an initial value "Coding For All"
+company = 'Coding For All'
+print(company)
+
+#Print the length of the company string using len() method and print()
+print(len(company))
+
+#Change all the characters to uppercase letters using upper() method.
+print(company.upper())
+
