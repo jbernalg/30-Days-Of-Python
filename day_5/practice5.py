8d95cb7b6cd627d208ad3159ca9aad7ecf7c803e
jbernalg <jeinffersonbernal@gmail.com>
2022-06-10 21:19:53 -0500

    practice5.3

--- day_5/practice5.py
+++ day_5/practice5.py
@@ -23,6 +23,24 @@ print('Vegetable: ', vegetables)
 print('Number of vegetables: ',len(vegetables))
 print('Animal Products: ', animal_products)
 print('Number of animalproducts: ',len(animal_products))
+print('Web Tecnologies: ',web_techs)
+print('Number of Web: ',len(web_techs))
+print('Countries: ', countries)
+print('Number of countries: ', len(countries))
+
+#List can have items of different data types
+lista = ['Asabeneh', 250, True, {'country':'Finland', 'city':'Helsinki'}]
+print(lista)
+
+#--------------Accessing list items using indexing----------------------------
+print(fruits[0])
+print(fruits[2])
+last_index = len(fruits) - 1
+print(last_index)
+
+print(fruits[-4])
+print(fruits[-2])
+
 
 
 
