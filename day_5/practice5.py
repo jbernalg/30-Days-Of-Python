0556b48789ee766a17a48907b7d746f914fdfc14
jbernalg <jeinffersonbernal@gmail.com>
2022-06-10 21:10:27 -0500

    practice5.2

--- day_5/practice5.py
+++ day_5/practice5.py
@@ -10,3 +10,20 @@ print(len(empty_list))
 lista = []
 print(len(lista))
 
+#List with initial values.
+fruits = ['banana', 'orange', 'mango', 'lemon']                     # list of fruits
+vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of vegetables
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
+web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+
+print('Fruits: ', fruits)
+print('Number of fruits: ', len(fruits))
+print('Vegetable: ', vegetables)
+print('Number of vegetables: ',len(vegetables))
+print('Animal Products: ', animal_products)
+print('Number of animalproducts: ',len(animal_products))
+
+
+
+
