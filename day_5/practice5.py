ae3a60448063688daa26e596f5918d167f77dde0
jbernalg <jeinffersonbernal@gmail.com>
2022-06-10 21:05:06 -0500

    practice5

---
+++ day_5/practice5.py
@@ -0,0 +1,12 @@
+#-----------------------Create a list------------------------------
+#list built in funtion
+lista = list()
+
+#empty list
+empty_list = list()
+print(len(empty_list))
+
+#squares brackets []
+lista = []
+print(len(lista))
+
