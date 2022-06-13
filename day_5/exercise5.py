420f05980237fda5e7896e2f29e589d4ca023801
jbernalg <jeinffersonbernal@gmail.com>
2022-06-13 18:50:58 -0500

    exercise5

---
+++ day_5/exercise5.py
@@ -0,0 +1,15 @@
+#Declare an empty list
+lista = list()
+
+#Declare a list with more than 5 items
+lista = [1,2,3,4,5]
+
+#Find the length of your list
+print(len(lista))
+
+#Get the first item, the middle item and the last item of the list
+print(lista[0])
+print(lista[2])
+print(lista[len(lista) - 1])
+
+
