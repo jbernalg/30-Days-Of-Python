f93e7da8c17fb6c4240f96645d83c95b37d894d2
jbernalg <jeinffersonbernal@gmail.com>
2022-06-13 20:35:13 -0500

    exercise5.2

--- day_5/exercise5.py
+++ day_5/exercise5.py
@@ -12,4 +12,11 @@ print(lista[0])
 print(lista[2])
 print(lista[len(lista) - 1])
 
+#Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
+mixed_data_types = ['Roberto', 'Aguilar', 1.72, 'soltero', 'Nicaragua']
+print(mixed_data_types)
+
+#Declare a list variable named it_companies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon.
+it_companies =['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print(it_companies)
 
