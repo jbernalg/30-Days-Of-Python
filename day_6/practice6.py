ba5fc32b1312d14448160dbbb45909bae93e1001
jbernalg <jeinffersonbernal@gmail.com>
2022-06-15 11:05:50 -0500

    practice6

---
+++ day_6/practice6.py
@@ -0,0 +1,16 @@
+#-----------------TUPLES-------------------------
+
+#----------------Create a Tuple-------------------
+#Empty tuple
+empty_tuple = ()
+print(empty_tuple)
+
+empty_tuple = tuple()
+print(empty_tuple)
+
+#tuple with initial values
+fruits = ('banana', 'orange', 'mango', 'lemon', 2)
+print(fruits)
+
+#---------------Tuple Length--------------------
+print(len(fruits))
