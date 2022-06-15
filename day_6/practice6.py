309908bea2da7fc7721ae136fbe46d88e6788f66
jbernalg <jeinffersonbernal@gmail.com>
2022-06-15 11:24:36 -0500

    practice6.3

--- day_6/practice6.py
+++ day_6/practice6.py
@@ -27,5 +27,28 @@ print(all_fruits)
 orange_mango = fruits[1:3]
 print(orange_mango)
 
+print('all fruits: ', fruits[-5:])
+print('orange mango: ', fruits[-4:-2])
+
+#---------------Changing tuples to list--------------------
+fruits = list(fruits)
+print(fruits)
+fruits = tuple(fruits)
+print(fruits)
+
+#----------------Checking an item in a tuple------------------
+print('orange' in fruits)
+print('apple' in fruits)
+
+#----------------Joining tuples----------------------
+fruits = ('banana', 'orange', 'mango', 'lemon', 2)
+vegetable = ('Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot')
+fruits_vegetable = fruits + vegetable
+print(fruits_vegetable)
+
+
+
+
+
 
 
