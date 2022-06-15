9e33ef74817f18ceca667704d9679b7fe26cba0f
jbernalg <jeinffersonbernal@gmail.com>
2022-06-15 11:12:52 -0500

    practice6.2

--- day_6/practice6.py
+++ day_6/practice6.py
@@ -14,3 +14,18 @@ print(fruits)
 
 #---------------Tuple Length--------------------
 print(len(fruits))
+
+#---------------Accessing Tuples items-------------------
+print('first fruit:', fruits[0])
+print('last fruits: ',fruits[len(fruits) - 1])
+print('first fruit: ', fruits[-5])
+print('last fruit: ', fruits[-1])
+
+#---------------Slicing tuples--------------------
+all_fruits = fruits[:] #all fruits
+print(all_fruits)
+orange_mango = fruits[1:3]
+print(orange_mango)
+
+
+
