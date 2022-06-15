e515beabcf6f9f9d3d640bbac8272fd15135e4b1
jbernalg <jeinffersonbernal@gmail.com>
2022-06-15 12:30:18 -0500

    exercise6.2

--- day_6/exercise6.py
+++ day_6/exercise6.py
@@ -8,8 +8,18 @@ print(family)
 
 #Join brothers and sisters tuples and assign it to siblings
 brothers = ('Manuel', 'Yeison', 'Jose')
-sisters = ('Jennifer', 'Salome')
+sisters = ('Jennifer', 'Salome') #las tuplas debe contener mas de un items
 siblings = brothers + sisters
 print(siblings) 
+
+#How many siblings do you have?
+print(len(siblings))
+
+#Modify the siblings tuple and add the name of your father and mother and assign it to family_members
+siblings = list(siblings)
+siblings.append('Maria')
+siblings.append('Jose')
+siblings = tuple(siblings)
+print(siblings)
 
 
