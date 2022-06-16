483fc7b3597aebe37427629a53ac6393d772cb13
jbernalg <jeinffersonbernal@gmail.com>
2022-06-16 18:22:04 -0500

    practice7

---
+++ day_7/practice7.py
@@ -0,0 +1,23 @@
+#-------------------SET----------------------
+
+#-----------Creating a Set-----------------
+st = {}
+print(st)
+st = set()
+print(st)
+
+#Creating a set with initial items
+fruits = {'bananas', 'orange', 'mango', 'lemon'}
+print(fruits)
+
+#----------Getting set's length------------
+print(len(fruits))
+
+#----------Accessing items in a set-----------
+#We use loop to access items
+
+#----------Checking an items----------------
+print('mango' in fruits)
+
+
+
