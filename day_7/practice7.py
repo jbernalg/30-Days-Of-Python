93c3de5ff7e337985c2e6177ce5161397bd8dfdb
jbernalg <jeinffersonbernal@gmail.com>
2022-06-16 18:39:57 -0500

    practice7.1

--- day_7/practice7.py
+++ day_7/practice7.py
@@ -19,5 +19,29 @@ print(len(fruits))
 #----------Checking an items----------------
 print('mango' in fruits)
 
+#----------Adding items to a set-----------
+#usind add()
+fruits.add('lime')
+print(fruits)
 
+#Using update(). Add multiple items
+fruits = {'bananas', 'orange', 'mango', 'lemon'}
+vegetables = {'tomato', 'potato', 'cabbage', 'onio', 'carrot'}
+fruits.update(vegetables)
+print(fruits)
+
+#----------Removing items from a set------------------
+#using remove(). To check the item exist in the set.
+fruits.remove('tomato')
+print(fruits)
+#fruits.remove('piña') #If the items is not found, the method will raise errors
+#print(fruits)
+
+#Using discard(). This method doesn't raise any errors
+fruits.discard('potato')
+print(fruits)
+fruits.discard('piña') #This method deosn't raise any errors if the item is not exist
+print(fruits)
+
+#Using pop() method. Remove a random item a list and it return the removed item
 
