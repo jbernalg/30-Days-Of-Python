8b29a514ebd8f1f8629429768d7e80d453ed0907
jbernalg <jeinffersonbernal@gmail.com>
2022-06-17 18:56:17 -0500

    exercise7

---
+++ day_7/exercise7.py
@@ -0,0 +1,19 @@
+#SETS
+it_companies = {'Facebbok', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+#Find the length of the set it_companies
+print(len(it_companies))
+
+#Add 'Twitter' to it_companies
+it_companies.add('Twitter')
+print(it_companies)
+
+#Insert multiple IT companies at once to the set it_companies
+new_companies = {'Whatsapp', 'Lenovo', 'Sony', 'Tik Tok'}
+it_companies.update(new_companies)
+print(it_companies)
+
+
