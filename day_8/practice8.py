64fcb9ff1fabd321fc29a781ac3e297078b3d71b
jbernalg <jeinffersonbernal@gmail.com>
2022-06-18 14:12:56 -0500

    practice8

---
+++ day_8/practice8.py
@@ -0,0 +1,25 @@
+#-------------DICTIONARY----------------
+
+#-------------Creating a dictionary----------------
+empty_dict = {}
+print(empty_dict)
+
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+        }
+    }
+
+print(person)
+
+#------------Dictionary length--------------
+print(len(person))
+
+
