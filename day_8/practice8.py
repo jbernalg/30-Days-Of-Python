ab3aa40ceccf548e276a5d0620298d0850c636b7
jbernalg <jeinffersonbernal@gmail.com>
2022-06-18 14:23:50 -0500

    practice8.2

--- day_8/practice8.py
+++ day_8/practice8.py
@@ -22,4 +22,26 @@ print(person)
 #------------Dictionary length--------------
 print(len(person))
 
+#------------Accessing dictionary items---------------
+#Using key name. We can access Dictionary items by referring to its key name.
+print(person['first_name'])
+print(person['country'])
+
+#Using get method
+#The get method returns None, which is a NoneType object data type, if the key does not exist.
+print(person.get('age'))
+print(person.get('color'))
+
+#------------Adding items to a dictionary----------------
+#adding new key and value pair
+person['job_title'] = 'Instructor'
+print(person.get('job_title'))
+
+#adding new value a key existent using append() method
+person['skills'].append('HTML')
+print(person['skills'])
+
+
+
+
 
