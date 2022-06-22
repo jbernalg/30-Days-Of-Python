3bb1932113bc6b3ea1341d5b4ef6b833876d03a6
jbernalg <jeinffersonbernal@gmail.com>
2022-06-22 16:14:49 -0500

    exercise9.4

--- day_9/exercise9.py
+++ day_9/exercise9.py
@@ -70,6 +70,19 @@ elif season == 'june' or season == 'july' or season == 'august':
 else:
     print('Enter a month correct')
 
+#The following list contains some fruits
+#If a fruit doesn't exist in the list add the fruit to the list and print the modified list. If the fruit exists print('That fruit already exist in the list')
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+
+fruit = input('Enter a fruit: '). lower()
+
+if fruit in fruits:
+    print('That fruit already exist in the list')
+else:
+    fruits.append(fruit)
+    print(fruits)
+
 
 
 
