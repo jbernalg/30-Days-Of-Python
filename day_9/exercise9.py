2fae5edb78cf5f992f2fbc30921b6e0510f43a0e
jbernalg <jeinffersonbernal@gmail.com>
2022-06-22 16:32:11 -0500

    exercise9.5

--- day_9/exercise9.py
+++ day_9/exercise9.py
@@ -83,9 +83,28 @@ else:
     fruits.append(fruit)
     print(fruits)
 
+#Here we have a person dictionary. Feel free to modify it!
+person={
+    'first_name': 'Asabeneh',
+    'last_name': 'Yetayeh',
+    'age': 250,
+    'country': 'Finland',
+    'is_marred': True,
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address': {
+        'street': 'Space street',
+        'zipcode': '02210'}
+    }
 
+#Check if the person dictionary has skills key, if so print out the middle skill in the skills list.
 
-
+if 'skills' in person:
+    list_skills = person['skills']
+    print(len(list_skills))
+    print(list_skills[1:2])
+    middle = (len(list_skills) - 1) // 2
+    print(middle)
+    print(list_skills[middle:middle + 1])
 
 
 
