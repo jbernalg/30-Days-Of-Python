4351f1ebe3e0ac4107c6cae296b048279d7476f3
jbernalg <jeinffersonbernal@gmail.com>
2022-06-23 11:57:21 -0500

    practice10.4

--- day_10/practice10.py
+++ day_10/practice10.py
@@ -55,4 +55,26 @@ numbers = (0,1,2,3,4,5)
 for number in numbers:
     print(number)
 
+#for loop with dictionary
+person = {
+    'first_name':'Asabeneh',
+    'last_name':'Yetayeh',
+    'age':250,
+    'country':'Finland',
+    'is_marred':True,
+    'skills':['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    'address':{
+        'street':'Space street',
+        'zipcode':'02210'
+    }
+}
+
+#keys
+for key in person:
+    print(key)
+
+#get both keys and values printed out 
+for key, value in person.items():
+    print(key, value)
+
 
