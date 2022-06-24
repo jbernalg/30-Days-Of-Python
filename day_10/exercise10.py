9b028b513524bcd037985208e60a97c4e8c0127b
jbernalg <jeinffersonbernal@gmail.com>
2022-06-24 10:55:29 -0500

    exercise10

---
+++ day_10/exercise10.py
@@ -0,0 +1,19 @@
+#----------------LOOPS----------------
+
+#Iterate 0 to 10 using for loop, do the same using while loop.
+for number in range(11):
+    print(number)
+
+number = 0
+while  number < 11:
+    print(number)
+    number += 1
+
+#Iterate 10 to 0 using for loop, do the same using while loop.
+for i in range(10,-1,-1):
+    print(i)
+
+number = 10
+while number > -1:
+    print(number)
+    number -= 1
