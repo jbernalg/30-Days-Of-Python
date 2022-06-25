a4067d46faccb7f1551a4b8054d305d6b4e513f0
jbernalg <jeinffersonbernal@gmail.com>
2022-06-25 12:55:39 -0500

    exercise10.7

--- day_10/exercise10.py
+++ day_10/exercise10.py
@@ -57,4 +57,19 @@ for i in range(101):
 count = 0
 for i in range(101):
     count += i
-print(count)
+print(f'The sum of all numbers is: {count}')
+
+#Use for loop to iterate from 0 to 100 and print the sum of all evens and the sum of all odds.
+evenSum = 0
+oddSum = 0
+for i in range(101):
+    if i % 2 == 0:
+        evenSum += i
+    else:
+        oddSum += i
+print(f'The sum of all evens is {evenSum}. The sum of all odds is {oddSum}')
+
+ 
+
+
+
