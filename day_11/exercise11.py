5c467a2f7abdbc2378cb143de3c5e52a55b2a266
jbernalg <jeinffersonbernal@gmail.com>
2022-06-29 11:27:29 -0500

    exercise11

---
+++ day_11/exercise11.py
@@ -0,0 +1,19 @@
+#---------------------FUNCTIONS--------------------------------
+
+#Declare a function add_two_numbers. It takes two parameters and it returns a sum.
+def add_two_numbers(num1, num2):
+    sum = num1 + num2
+    return sum
+print(add_two_numbers(12, 16))
+
+#Area of a circle is calculated as follows: area = π x r x r. Write a function that calculates area_of_circle.
+def area_of_circle(r):
+    area = 3.14*r**2
+    return area
+print(area_of_circle(12))
+
+ 
+
+
+
+
