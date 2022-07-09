81f695625ef845af323dfd1507f7c4270f88c9f2
jbernalg <jeinffersonbernal@gmail.com>
2022-07-09 13:54:57 -0500

    practice12.2

--- day_12_Modules/mymodule.py
+++ day_12_Modules/mymodule.py
@@ -2,3 +2,8 @@
 
 def generate_full_name(first_name, last_name):
     return first_name + ' ' + last_name
+
+def sum_two_nums(num1, num2):
+    return num1 + num2
+
+gravity = 9.8
--- day_12_Modules/practice12.py
+++ day_12_Modules/practice12.py
@@ -4,4 +4,15 @@
 #-------------------Create a Module-------------------
 #To create a module we write our codes in a python script and we save it as a .py file.
 import mymodule
+
 print(mymodule.generate_full_name('Jose', 'Gimenez'))
+
+#------------------Import Function from a Module-------------------
+from mymodule import generate_full_name, sum_two_nums, gravity
+
+print(generate_full_name('Daniel', 'Pereira'))
+print(sum_two_nums(20, 18))
+
+mass = 100
+weight = mass * gravity
+print(weight)
