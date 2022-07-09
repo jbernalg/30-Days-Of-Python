1a96fefe3fdc87d6fe5e3da2e913684903cc934b
jbernalg <jeinffersonbernal@gmail.com>
2022-07-09 13:47:38 -0500

    practice12

---
+++ day_12_Modules/mymodule.py
@@ -0,0 +1,4 @@
+
+
+def generate_full_name(first_name, last_name):
+    return first_name + ' ' + last_name
---
+++ day_12_Modules/practice12.py
@@ -0,0 +1,7 @@
+#-------------------MODULES----------------------
+#A module is a file containing a set of codes or a set of functions which can be included to an application
+
+#-------------------Create a Module-------------------
+#To create a module we write our codes in a python script and we save it as a .py file.
+import mymodule
+print(mymodule.generate_full_name('Jose', 'Gimenez'))
