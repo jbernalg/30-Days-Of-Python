5d121131312e10689f7dc20bfd513505935dad14
jbernalg <jeinffersonbernal@gmail.com>
2022-07-15 10:39:22 -0500

    practice12.3

--- day_12_Modules/practice12.py
+++ day_12_Modules/practice12.py
@@ -16,3 +16,33 @@ print(sum_two_nums(20, 18))
 mass = 100
 weight = mass * gravity
 print(weight)
+
+#------------------Import Function from a Module and Renaming----------------
+#During importing we can rename the name of the module
+from mymodule import generate_full_name as fullname, sum_two_nums as total, gravity as g
+print(fullname('Manuel', 'Girot'))
+print(total(12, 20))
+
+mass = 14
+weight = mass * g
+print(weight)
+
+
+#-----------------Import Built-in Modules------------------
+#math, datetime, os, sys, random, statistics, collections, json...
+
+#-----------------OS Import------------------
+#Using os module it is possible to automatically perform many operating system task.
+import os
+
+#Creating a directory
+os.mkdir('directory_name')
+
+#Changing the current directory
+#os.chdir('path')
+
+#Getting current working directory
+print(os.getcwd())
+
+#Removing a directory
+os.rmdir('directory_name')
