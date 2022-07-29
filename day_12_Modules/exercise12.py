060793ec403c3c75a619f60322ad0e04b49ed015
jbernalg <jeinffersonbernal@gmail.com>
2022-07-29 17:33:06 -0500

    practice12.10

--- day_12_Modules/exercise12.py
+++ day_12_Modules/exercise12.py
@@ -14,4 +14,21 @@ def random_user_id():
 
 print(random_user_id())
 
+#Modify the previous task. Declare a function named user_id_gen_by_user. It doesn’t take any parameters but it takes two inputs using input(). One of the inputs is the number of characters and the second input is the number of IDs which are supposed to be generated.
+def random_user_id2(cant):
+    data = list(string.ascii_lowercase) + list(string.digits)
+    codigo = ''
 
+    for i in range(cant):
+        codigo = codigo + (random.choice(data))
+    return codigo
+
+
+def user_id_gen_by_user():
+    tam = int(input())
+    element = int(input())
+    
+    for i in range(element):
+        print(random_user_id2(tam))
+
+print(user_id_gen_by_user())
