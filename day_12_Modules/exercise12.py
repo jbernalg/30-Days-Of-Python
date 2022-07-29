24fbd10b6a96adfcb05ab81063a2222179bb32f4
jbernalg <jeinffersonbernal@gmail.com>
2022-07-29 18:49:22 -0500

    practice12.12

--- day_12_Modules/exercise12.py
+++ day_12_Modules/exercise12.py
@@ -44,6 +44,17 @@ def rgb_color_gen():
 
 print(rgb_color_gen())
 
+#Write a function list_of_hexa_colors which returns any number of hexadecimal colors in an array (six hexadecimal numbers written after #. Hexadecimal numeral system is made out of 16 symbols, 0-9 and first 6 letters of the alphabet, a-f. Check the task 6 for output examples).
+def list_of_hexa_colors():
+    hexa = list(string.digits) + ['a','b','c','d','e','f']
+    colors = ''
+
+    for i in range(6):
+        colors = colors + random.choice(hexa)
+
+    return f'#{colors}'
+
+print(list_of_hexa_colors())
 
 
 
