4f07960c0c75ffafbe4c72a1c71e256e305f823d
jbernalg <jeinffersonbernal@gmail.com>
2022-08-03 17:01:01 -0500

    exercise13.8

--- day_13_List_Comprehension/exercise13.py
+++ day_13_List_Comprehension/exercise13.py
@@ -77,3 +77,7 @@ sentence = 'On a summer day somner smith went simming in the sun and his red ski
 examine = sentence.split()
 result = [n for n in examine if len(n) >= 4]
 print(result)
+
+# Use a nested list comprehension to find all of the numbers from 1-100 that are divisible by any single digit besides 1 (2-9)
+result = [n for n in range(1,100) if True in [True for x in range(2,10) if n % x  == 0]]
+print(result)
