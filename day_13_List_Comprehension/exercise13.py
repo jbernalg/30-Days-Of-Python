6034d02409c747c3a4442c3cb342b0e952ac28f3
jbernalg <jeinffersonbernal@gmail.com>
2022-08-03 15:08:21 -0500

    exercise13.2

--- day_13_List_Comprehension/exercise13.py
+++ day_13_List_Comprehension/exercise13.py
@@ -4,3 +4,10 @@
 numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
 filter_numbers = [i for i in numbers if i <= 0]
 print(filter_numbers)
+
+#Flatten the following list of lists of lists to a one dimensional list :
+#[number for row in list_of_list for number in row]
+list_of_list =[[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
+new_list = [number for row in list_of_list for number in row]
+new_list2 = [number for row in new_list for number in row]
+print(new_list2)
