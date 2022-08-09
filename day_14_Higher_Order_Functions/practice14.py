2c11e3151484868ac48f0a1e7f851f50f74172f6
jbernalg <jeinffersonbernal@gmail.com>
2022-08-09 18:33:04 -0500

    practice 13.5

--- day_14_Higher_Order_Functions/practice14.py
+++ day_14_Higher_Order_Functions/practice14.py
@@ -119,3 +119,15 @@ def greeting():
 
 print(greeting())
 
+# -----------------Accepting parameters in decorator functions--------------------
+def decorator_with_parameters(function):
+    def wrapper_accepting_parameters(para1,para2,para3):
+        function(para1,para2,para3)
+        print('I live in {}'.format(para3))
+    return wrapper_accepting_parameters
+
+@decorator_with_parameters
+def print_full_name(first_name, last_name,country):
+    print('I am {} {}. I love to teach.'.format(first_name, last_name, country))
+
+print_full_name('Jeinfferson', 'Bernal', 'Venezuela')
