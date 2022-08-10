013f7de86e7341228201519b9e44128f7ec8443f
jbernalg <jeinffersonbernal@gmail.com>
2022-08-10 10:10:31 -0500

    exercise 13.2

--- day_14_Higher_Order_Functions/exercise14.py
+++ day_14_Higher_Order_Functions/exercise14.py
@@ -1,6 +1,14 @@
-#-------------------Exercise Higher Order Function----------------------
+# -------------------Exercise Higher Order Function----------------------
 
-#Explain the difference between map, filter and reduce
+# Explain the difference between map, filter and reduce
 # map function applies the function passed as a parameters over all the items of the list. It return a iterable
 # filter function allows you to filter the items of the list  by means of a function. Iy return a iterable
 # reduce function applies the function passed as a parameters  over all the items and return a single value
+
+# Explain the difference between higher order function, closure and decorator
+# The Higher Order Function is a function that operates with another function or functions.
+# It contains other functions as input parameters or it return a function as output.
+# Closures are internal functions that are enclosed  within the external function.
+# It can access variables present in the scope of the external function
+# Decorator allows to add new functionality to an existing object without modifying its structure
+
