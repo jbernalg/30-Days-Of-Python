617122ba51fb49bff5a0ba990aed650dd090a95b
jbernalg <jeinffersonbernal@gmail.com>
2022-08-27 09:42:22 -0500

    exercise 16.3

--- day_16_Python_DataTime/exercise16.py
+++ day_16_Python_DataTime/exercise16.py
@@ -20,3 +20,9 @@ print('timestamp', timestamp)
 # Format the current date using this format: "%m/%d/%Y, %H:%M:%S")
 t = now.strftime('%m/%d/%Y, %H:%M:%S')
 print(t)
+
+# Today is 5 December, 2019. Change this time string to time.
+date_string = '5 December, 2019'
+
+date_object = datetime.strptime(date_string, '%d %B, %Y')
+print('date: ', date_object)
