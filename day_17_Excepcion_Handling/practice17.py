82552d8b7d6ca4fd380077ab6a5b3a8691a9b1b5
jbernalg <jeinffersonbernal@gmail.com>
2022-08-27 10:59:06 -0500

    practice 17.2

--- day_17_Excepcion_Handling/practice17.py
+++ day_17_Excepcion_Handling/practice17.py
@@ -15,8 +15,17 @@
 # finally:
 #   {Always run this code}
 
-# example:
+# example 1:
 try:
     print(10 + '5')
+except:
+    print('Something went wrong')
+
+# example 2:
+try:
+    name = input('Enter your name: ')
+    year_born = input('year you were born: ')
+    age = 2022 - year_born
+    print(f'You are {name}. And you age is {age}')
 except:
     print('Something went wrong')
