731f8a44cc599c3acb26c92e02274032333d2724
jbernalg <jeinffersonbernal@gmail.com>
2022-08-27 11:09:54 -0500

    practice 17.3

--- day_17_Excepcion_Handling/practice17.py
+++ day_17_Excepcion_Handling/practice17.py
@@ -29,3 +29,33 @@ try:
     print(f'You are {name}. And you age is {age}')
 except:
     print('Something went wrong')
+
+# example 3:
+try:
+    name = input('Enter your name: ')
+    year_born = input('year you were born: ')
+    age = 2022 - year_born
+    print(f'You are {name}. And you age is {age}')
+except TypeError:
+    print('Type error occured')
+except ValueError:
+    print('Value error occured')
+except ZeroDivisionError:
+    print('zero division error occured')
+
+# example 4:
+try:
+    name = input('Enter your name: ')
+    year_born = input('year you were born: ')
+    age = 2022 - int(year_born)
+    print(f'You are {name}. And you age is {age}')
+except TypeError:
+    print('Type error occured')
+except ValueError:
+    print('Value error occured')
+except ZeroDivisionError:
+    print('zero division error occured')
+else:
+    print('I usually run with the try block')
+finally:
+    print('I always run')
