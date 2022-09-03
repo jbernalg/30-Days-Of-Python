edff37beea3b78f68b2a3a75740306bedbde03d7
jbernalg <jeinffersonbernal@gmail.com>
2022-09-02 19:28:40 -0500

    practice 18.6

--- day_18_Regular_Expressions/practice18.py
+++ day_18_Regular_Expressions/practice18.py
@@ -55,4 +55,19 @@ print(matches)
 # If we do not have the re.I flag , then will have to write our pattern differently
 
 matches = re.findall('Python|python', txt)
-print(matches) 
+print(matches)
+
+matches = re.findall('[Pp]ython', txt)
+print(matches)
+
+#------------Replacing a substring-----------------
+match_replaced = re.sub('Python|python', 'Javascript', txt, re.I)
+print(match_replaced)
+
+txt = '''%I a%m te%%a%%che%r% a%n%d %% I l%o%ve te%ach%ing. 
+T%he%re i%s n%o%th%ing as r%ewarding a%s e%duc%at%i%ng a%n%d e%m%p%ow%er%ing p%e%o%ple.
+I fo%und te%a%ching m%ore i%n%t%er%%es%ting t%h%an any other %jobs. 
+D%o%es thi%s m%ot%iv%a%te %y%o%u to b%e a t%e%a%cher?'''
+
+matches = re.sub('%', '', txt)
+print(matches)
