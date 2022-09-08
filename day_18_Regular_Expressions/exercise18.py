49497130b5f77e1c028a1a3cd85d3fd7c5346c4a
jbernalg <jeinffersonbernal@gmail.com>
2022-09-08 09:55:26 -0500

    exercise 18.4

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -31,3 +31,15 @@ print(text_match('ac'))
 print(text_match('abc'))
 print(text_match('a'))
 print(text_match('abb'))
+
+# Write  a program that matches a string that has an followed by one or more b's
+def text_match(text):
+    patterns = 'ab+?'
+    if re.search(patterns, text):
+        return 'Found a match!'
+    else:
+        return 'Not matched!'
+
+print(text_match('ab'))
+print(text_match('acb'))
+
