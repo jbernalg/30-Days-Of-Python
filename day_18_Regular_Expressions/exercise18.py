fe4aad9cd7abdb027d679700e62b7925a81f5acb
jbernalg <jeinffersonbernal@gmail.com>
2022-09-08 10:10:05 -0500

    exercise 18.7

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -65,3 +65,14 @@ def text_match(text):
 
 print(text_match('abbb'))
 print(text_match('abbcbbb'))
+
+# Write a Python program that matches a string that has an a followed by two to three 'b'
+def text_match(text):
+    patterns = 'ab{2,3}?'
+    if re.search(patterns, text):
+        return 'Found a match!'
+    else:
+        return 'Not matched!'
+
+print(text_match('ab'))
+print(text_match('aabbac'))
