f112e3ad7ce40677e7eb814c48847afd86f542a8
jbernalg <jeinffersonbernal@gmail.com>
2022-09-08 10:06:44 -0500

    exercise 18.6

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -54,3 +54,14 @@ def text_match(text):
 print(text_match('ab'))
 print(text_match('abbb'))
 print(text_match('a'))
+
+# Write a Python program that matches a string that has an a followed by three 'b'
+def text_match(text):
+    patterns = 'ab{3}?'
+    if re.search(patterns, text):
+        return 'Found a match!'
+    else:
+        return 'Not matched!'
+
+print(text_match('abbb'))
+print(text_match('abbcbbb'))
