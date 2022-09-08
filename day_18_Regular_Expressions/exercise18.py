8b2b6a12208957e1a10a4fc18c7e6f9be0b7b24a
jbernalg <jeinffersonbernal@gmail.com>
2022-09-08 10:02:51 -0500

    exercise 18.5

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -43,3 +43,14 @@ def text_match(text):
 print(text_match('ab'))
 print(text_match('acb'))
 
+# Write a Python program that matches a string that has an a followed by zero or one 'b'.
+def text_match(text):
+    patterns = 'ab?'
+    if re.search(patterns, text):
+        return 'Found a match!'
+    else:
+        return 'Not matched!'
+
+print(text_match('ab'))
+print(text_match('abbb'))
+print(text_match('a'))
