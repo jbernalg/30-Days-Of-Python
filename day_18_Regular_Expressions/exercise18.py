a195482450407a41f2db67b326a84293dfa018ac
jbernalg <jeinffersonbernal@gmail.com>
2022-09-08 09:49:57 -0500

    exercise 18.3

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -19,5 +19,15 @@ def specific_char(string):
 print(specific_char('hola 123'))
 print(specific_char('% & $'))
 
+# Write  a program that matches a string that has an followed by zero or more b's
+def text_match(text):
+    patterns = '^a(b*)$'
+    if re.search(patterns, text):
+        return 'Found a match!'
+    else:
+        return 'Not matched!'
 
-
+print(text_match('ac'))
+print(text_match('abc'))
+print(text_match('a'))
+print(text_match('abb'))
