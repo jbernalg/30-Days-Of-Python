bdf43df148c727b4fbe4512b38b5e64ead75e2af
jbernalg <jeinffersonbernal@gmail.com>
2022-09-09 14:57:08 -0500

    exercise 18.10

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -98,3 +98,14 @@ def text_match(text):
 
 print(text_match('Python'))
 print(text_match('python'))
+
+# Write a Python program that matches a string that has an 'a' followed by anything, ending in 'b'
+def text_match(text):
+   patterns = 'a.*?b$'
+   if re.search(patterns, text):
+       return 'Found a match!'
+   else:
+       return 'Not matched!'
+
+print(text_match('cfbabdb'))
+print(text_match('abaaaccc'))
