5aa352b06600d47d28b96ba3c45cbcc3938ae821
jbernalg <jeinffersonbernal@gmail.com>
2022-09-09 15:02:07 -0500

    exercise 18.11

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -109,3 +109,15 @@ def text_match(text):
 
 print(text_match('cfbabdb'))
 print(text_match('abaaaccc'))
+
+# Write a Python program that matches a word at the beginning of a string.
+def text_match(text):
+   patterns = '^\w+'
+   if re.search(patterns, text):
+       return 'Found a match!'
+   else:
+       return 'Not matched!'
+
+print(text_match('The quick brown'))
+print(text_match(' the quick brown'))
+
