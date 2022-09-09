ef59429d43510ab5c6a004f0cf70a06587d7e857
jbernalg <jeinffersonbernal@gmail.com>
2022-09-09 15:19:39 -0500

    exercise 18.15

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -155,3 +155,13 @@ print(text_match('The quick brown the lazy'))
 print(text_match('The zafari'))
 print(text_match('The ratioz'))
 
+# Write a Python program to match a string that contains only upper and lowercase letters, numbers, and underscores
+def text_match(text):
+   patterns = '^[a-zA-Z0-9_]*$'
+   if re.search(patterns, text):
+       return 'Found a match!'
+   else:
+       return 'Not matched!'
+
+print(text_match('The quick brown the lazy'))
+print(text_match('Python_exercise_12'))
