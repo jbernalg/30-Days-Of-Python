4c163301617dc2cccaddf20c6ca3950201f72a24
jbernalg <jeinffersonbernal@gmail.com>
2022-09-09 15:08:26 -0500

    exercise 18.13

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -131,3 +131,14 @@ def text_match(text):
 
 print(text_match('The quick brown.'))
 print(text_match('The quick brown '))
+
+# Write a Python program that matches a word containing 'z'.
+def text_match(text):
+   patterns = '\w*z.\w*'
+   if re.search(patterns, text):
+       return 'Found a match!'
+   else:
+       return 'Not matched!'
+
+print(text_match('The quick brown the lazy'))
+print(text_match('Python exercise'))
