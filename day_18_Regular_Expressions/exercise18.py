5505bc9d9c2fb37b9b6f913732c8424afb19155d
jbernalg <jeinffersonbernal@gmail.com>
2022-09-09 15:05:02 -0500

    exercise 18.12

--- day_18_Regular_Expressions/exercise18.py
+++ day_18_Regular_Expressions/exercise18.py
@@ -121,3 +121,13 @@ def text_match(text):
 print(text_match('The quick brown'))
 print(text_match(' the quick brown'))
 
+# Write a Python program that matches a word at the end of string, with optional punctuation
+def text_match(text):
+   patterns = '\w+\S*$'
+   if re.search(patterns, text):
+       return 'Found a match!'
+   else:
+       return 'Not matched!'
+
+print(text_match('The quick brown.'))
+print(text_match('The quick brown '))
