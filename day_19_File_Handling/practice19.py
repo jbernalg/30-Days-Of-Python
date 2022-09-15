f46f01db0d53b93fd1ac0129ca64893e63cd7e38
jbernalg <jeinffersonbernal@gmail.com>
2022-09-15 12:32:15 -0500

    practice 19.11

--- day_19_File_Handling/practice19.py
+++ day_19_File_Handling/practice19.py
@@ -73,3 +73,28 @@ if os.path.exists('./example.txt'):
     os.remove('./exmple.txt')
 else:
     print('The file does not exist')
+
+# ---------------File Types-----------------
+
+# --------------File with txt extension----------------
+# File with JSON extension
+# JSON stands for JavaScript Object Notation. Actually, it is a stringified JavaScript object or Python dictionary.
+# dictionary
+person_dct= {
+    "name":"Asabeneh",
+    "country":"Finland",
+    "city":"Helsinki",
+    "skills":["JavaScrip", "React","Python"]
+}
+# JSON: A string form a dictionary
+person_json = "{'name': 'Asabeneh', 'country': 'Finland', 'city': 'Helsinki', 'skills': ['JavaScrip', 'React', 'Python']}"
+
+# we use three quotes and make it multiple line to make it more readable
+person_json = '''{
+    "name":"Asabeneh",
+    "country":"Finland",
+    "city":"Helsinki",
+    "skills":["JavaScrip", "React","Python"]
+}'''
+ 
+# Changing JSON to dictionary
