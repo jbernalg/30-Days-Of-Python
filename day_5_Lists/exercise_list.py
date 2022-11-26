c61826f10c12e8bb48aefd67110353922e359f9a
jbernalg <jeinffersonbernal@gmail.com>
2022-11-26 14:52:11 -0500

    exercise list 5.2

--- day_5_Lists/exercise_list.py
+++ day_5_Lists/exercise_list.py
@@ -1,15 +1,31 @@
 #-----------------------Ejecicios---------------------
 
 # 1. escriba un programa que sume todos los elementos de una lista
-lista_numbers = [1,5,7,9]
-count = 0
+def sum_list(lista):
+    count = 0
 
-for i in lista_numbers:
-    count += i
-print(count)
+    for i in lista:
+        count += i
+    return count
+
+print(sum_list([2,3,4]))
 
 # 2. Escriba un programa en Python para multiplicar todos los elementos de una lista
-mult = 1
-for i in lista_numbers:
-    mult *= i
-print(mult)
+def mult_list(lista):
+    mult = 1
+
+    for i in lista:
+        mult *= i
+    return mult
+
+print(mult_list([2,5,2]))
+
+# Escriba un programa en Python para obtener el número más grande de una lista.
+def max_list(lista):
+    max = lista[0]
+    for i in lista:
+        if i > max:
+            max = i
+    return max
+
+print(max_list([3,-2,0,10,4]))
