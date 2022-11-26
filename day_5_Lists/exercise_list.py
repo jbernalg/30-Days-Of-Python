08bf69cde5f0531bd4b1175cc96a4ec57240d3de
jbernalg <jeinffersonbernal@gmail.com>
2022-11-26 14:44:22 -0500

    exercise list 5

---
+++ day_5_Lists/exercise_list.py
@@ -0,0 +1,15 @@
+#-----------------------Ejecicios---------------------
+
+# 1. escriba un programa que sume todos los elementos de una lista
+lista_numbers = [1,5,7,9]
+count = 0
+
+for i in lista_numbers:
+    count += i
+print(count)
+
+# 2. Escriba un programa en Python para multiplicar todos los elementos de una lista
+mult = 1
+for i in lista_numbers:
+    mult *= i
+print(mult)
