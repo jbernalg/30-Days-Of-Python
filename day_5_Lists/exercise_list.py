26e268d3858f4dada5822f5d78349bdd38a5a537
jbernalg <jeinffersonbernal@gmail.com>
2022-11-26 15:04:42 -0500

    exercise list 5.3

--- day_5_Lists/exercise_list.py
+++ day_5_Lists/exercise_list.py
@@ -29,3 +29,27 @@ def max_list(lista):
     return max
 
 print(max_list([3,-2,0,10,4]))
+
+#Escriba un programa en Python para obtener el número más pequeño de una lista
+def min_list(lista):
+    min = lista[0]
+
+    for i in lista:
+        if i < min:
+            min = i
+    return min
+
+print(min_list([4,2,8,0,1]))
+
+# Escriba un programa en Python para contar el número de cadenas donde la longitud de la 
+# cadena es 2 o más y el primer y último carácter de la cadena son el mismo.
+def match_word(words):
+    match = 0
+
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]:
+            match += 1
+    return match
+
+print(match_word(['abc', 'xyz', 'aba', '1221']))
+
