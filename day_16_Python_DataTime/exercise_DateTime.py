a2344d0199239c4b982fe2d4cc376b0f449b24c7
jbernalg <jeinffersonbernal@gmail.com>
2022-11-29 09:50:05 -0500

    exercise datetime 1.2

--- day_16_Python_DataTime/exercise_DateTime.py
+++ day_16_Python_DataTime/exercise_DateTime.py
@@ -23,8 +23,22 @@ print('dias del año: ', datetime.date.today().strftime('%j'))
 # f) Numero del dia de la semana
 print('numero del dia de la semana: ', datetime.date.today().strftime('%w'))
 
-# e) dia del mes
+# g) dia del mes
 print('dia del mes: ', datetime.date.today().strftime('%d'))
 
-# f) dia de la semana
+# h) dia de la semana
 print('dia de la semana: ', datetime.date.today().strftime('%A'))
+
+# 2. Escriba un programa en Python para determinar si un año dado es un año bisiesto.
+def año_bisiesto(año):
+    if año % 400 == 0:
+        return True
+    if año % 100 == 0:
+        return False
+    if año % 4 == 0:
+        return True
+    else:
+        return False
+
+result = año_bisiesto(1995)
+print(result)
