2e6c319c4b1a368ae60f9ddfafa3588b12d52ed9
jbernalg <jeinffersonbernal@gmail.com>
2022-12-07 07:19:36 -0500

    Reto 1.5

--- day_Reto_01/charts.py
+++ day_Reto_01/charts.py
@@ -7,3 +7,17 @@ def generate_bar_chart(labels, values):
     ax.bar(labels, values)
     plt.show()
 
+
+# funcion grafico de pie
+def generate_pie_chart(labels, values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    ax.axis('equal')
+    plt.show()
+
+
+if __name__ == '__main__':
+    
+    labels = ['a', 'b', 'c']
+    values = [20, 50, 30]
+    generate_pie_chart(labels, values)
