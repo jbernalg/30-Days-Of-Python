7271c100cd6a72e6b765b18200f9ba7794c420ee
jbernalg <jeinffersonbernal@gmail.com>
2023-08-29 09:42:57 -0500

    Exercise Practice 1.2

--- Practice_Exercise/exercises_01.py
+++ Practice_Exercise/exercises_01.py
@@ -59,3 +59,46 @@ Area = 3.8013271108436504
 sequence = '1,4,6,78,5,21'
 print(list(sequence.split(',')))
 print(tuple(sequence.split(',')))
+
+# Exercise 7
+# Write a Python program that accepts a filename from the user and prints 
+# the extension of the file.
+# Sample filename : abc.java
+# Output : java
+#filename = input('Ingrese el nombre del archivo junto a su extension: ')
+#lista = list(filename.split('.'))
+#print(lista[-1])
+
+# Exercise 8
+# Write a Python program to display the first and last colors from the following list.
+color_list = ["Red","Green","White" ,"Black"]
+print(color_list[0],color_list[-1])
+
+# Exercise 9
+'''
+Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+exam_st_date = (11, 12, 2014)
+Sample Output : The examination will start from : 11 / 12 / 2014
+'''
+exam_st_date = (11, 12, 2014)
+print(f'{exam_st_date[0]} / {exam_st_date[1]} / {exam_st_date[2]}')
+
+# Solucion alternativa
+print('The examination will start from: %i / %i / %i'%exam_st_date)
+
+# Exercise 10
+# Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
+# Sample value of n is 5
+# Expected Result : 615
+n1 = input('Ingrese un entero: ')
+n2 = n1 + n1
+n3 = n1 + n1 + n1
+result = int(n1) + int(n2) + int(n3)
+print(result)
+
+# solucion alternativa
+n = int(input('Ingrese un entero: '))
+n1 = int('%s'%n)
+n2 = int('%s%s'%(n,n))
+n3 = int('%s%s%s'%(n,n,n))
+print(n1+n2+n3)
