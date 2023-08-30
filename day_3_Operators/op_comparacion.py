fedf2f7acdb84daeeeb2ad1146d825ad913ccebe
jbernalg <jeinffersonbernal@gmail.com>
2023-08-30 09:21:45 -0500

    repaso operadores comparacion

---
+++ day_3_Operators/op_comparacion.py
@@ -0,0 +1,31 @@
+#--------- OPERADORES DE COMPARACION -----------------
+
+# igual que (==)
+igual_que = 5 == 6
+
+# distinto de (!=)
+distinto_de = 5!= 6
+
+# mayor que (>)
+mayor_que = 5 > 6
+
+# menor que (<)
+menor_que = 5 < 6
+
+# mayor o igual (>=)
+mayor_o_igual = 5 >= 6
+
+# menor o igual (<=)
+menor_o_igual = 5 <= 6
+
+# calculos combinados
+a = 10
+b = 5
+c = 20
+comparacion = a + b <= c
+
+# comparacion usuarios
+contrasena_bd = 'Jose Miguel'
+contrasena_escrita = 'Jose'
+
+print(contrasena_bd == contrasena_escrita)
