49fa566114601a6f21abfac9b202730d78b4f135
jbernalg <jeinffersonbernal@gmail.com>
2023-08-30 09:05:41 -0500

    repaso operadores aritmeticos

---
+++ day_3_Operators/operadores.py
@@ -0,0 +1,33 @@
+#------------- OPERADORES ARITMETICOS --------------------
+
+# Suma
+suma = 12 + 5
+print(suma)
+
+# Resta
+resta = 12 - 5
+print(resta)
+
+# Multiplicacion
+multi = 12*5
+print(multi)
+
+# Division
+division = 12 / 5 # devuelve un float
+print(division)
+
+# Potenciacion
+exponente = 12**5
+print(exponente)
+
+# Division baja
+division_baja = 12 // 5 # devuelve un entero redondeado hacia abajo
+print(division_baja)
+
+# Resto o modulo
+resto = 12 % 5
+print(resto)
+
+# Mostrar el tipo de dato
+tipo_de_dato = type({"hola", 23, False})
+print(tipo_de_dato)
