554a240b2af3ac2561b1d5e8e76c93438c07ac5f
jbernalg <jeinffersonbernal@gmail.com>
2023-09-01 08:20:10 -0500

    Reto 12

---
+++ Retos/reto_12.py
@@ -0,0 +1,23 @@
+'''
+ Crea una función que reciba dos cadenas como parámetro (str1, str2) e imprima otras 
+ dos cadenas como salida (out1, out2).
+ - out1 contendrá todos los caracteres presentes en la str1 pero NO estén presentes en str2.
+ - out2 contendrá todos los caracteres presentes en la str2 pero NO estén presentes en str1.
+'''
+
+texto1 = 'hola'
+texto2 = 'mundo'
+
+def del_character_common(texto1, texto2):
+    
+    set1 = set(texto1)
+    set2 = set(texto2)
+    set_inter = set1.intersection(set2)
+
+    for elem in set_inter:
+        salida1 = texto1.replace(elem,'')
+        salida2 = texto2.replace(elem,'')
+
+    return salida1, salida2
+
+print(del_character_common(texto1, texto2))
