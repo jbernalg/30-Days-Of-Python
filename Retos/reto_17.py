b7de52bf176083f22130eca7e9d19c6550ec4d68
jbernalg <jeinffersonbernal@gmail.com>
2023-09-05 06:46:59 -0500

    Reto 17 solucionado!

--- Retos/reto_17.py
+++ Retos/reto_17.py
@@ -3,29 +3,39 @@ Crea una función que reciba un String de cualquier tipo y se encargue de poner 
 la primera letra de cada palabra.
 - No se pueden utilizar operaciones del lenguaje que lo resuelvan directamente.
 '''
-# Base de Datos del abecedario
-minus_mayus = {'a':'A', 'b':'B', 'c':'C', 'd':'D', 'e': 'E', 'f':'F', 'g':'G', 'h':'H',
-               'i':'I', 'j':'J', 'k':'K', 'l':'L', 'm':'M', 'n':'N', 'ñ':'Ñ', 'o':'O',
-               'p':'P', 'q':'Q', 'r':'R', 's':'S', 't':'T', 'u':'U', 'v':'V', 'w':'W',
-               'x':'X', 'y':'Y', 'z':'Z'}
 
-string = ' saludos d'
-#string[0]= 'A'
-print(string)
+def Upper_First_Letter(string):
 
-if string[0] in minus_mayus:
-    string = string.replace(string[0], minus_mayus[string[0]])
+    # Base de Datos del abecedario
+    minus_mayus = {'a':'A', 'b':'B', 'c':'C', 'd':'D', 'e': 'E', 'f':'F', 'g':'G', 'h':'H',
+                   'i':'I', 'j':'J', 'k':'K', 'l':'L', 'm':'M', 'n':'N', 'ñ':'Ñ', 'o':'O',
+                   'p':'P', 'q':'Q', 'r':'R', 's':'S', 't':'T', 'u':'U', 'v':'V', 'w':'W',
+                   'x':'X', 'y':'Y', 'z':'Z'}
 
+    # convertimos el texto en una lista
+    list_text = list(string)
 
+    # cambiar la primera letra del texto
+    if list_text[0] in minus_mayus:
+        list_text[0] = minus_mayus[string[0]]
 
-for i in range(len(string)):
+    # recorrer toda la lista y reeplazamos la letra de cada inicio de texto
+    for i in range(len(list_text)-1):
     
-    if string[i] == ' ' and i+1 < (len(string)-1):
+        if list_text[i] == ' ' and i+1 <= (len(list_text)-1):
 
-        if string[i+1] in minus_mayus:
-            #string[i+1] = minus_mayus[string[i+1]]
-            print(string)
+            if list_text[i+1] in minus_mayus:
+                list_text[i+1] = minus_mayus[list_text[i+1]]
          
-print(string)
-   
-    
+    # convertimos la lista a texto
+    string = ''.join(list_text)
+
+    return string
+
+# test
+print(Upper_First_Letter(' hola 8undo d'))
+print(Upper_First_Letter('programacion en python'))
+print(Upper_First_Letter('! /cada cuanto tienes  hambre'))
+# ' hola 8undo d' -->  Hola 8undo D 
+# 'programacion en python' -->   Programacion En Python   
+# '! /cada cuanto tienes  hambre' -->   ! /cada Cuanto Tienes  Hambre 
