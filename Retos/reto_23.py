a5c0d0da7d8add17681740f2980a7324e11fb3d6
jbernalg <jeinffersonbernal@gmail.com>
2023-09-07 08:33:18 -0500

    Reto 23 solucionado

--- Retos/reto_23.py
+++ Retos/reto_23.py
@@ -19,6 +19,37 @@ list1 = [1, 3, 5, 7, 9]
 list2 = [1, 2, 3, 4, 5]
 band = False
 
+#### Test
 print(sets_arrays(list1, list2, band)) # --> [2, 4, 7, 9] 
 print(sets_arrays([1,4,6,7], [1,3,5,8], True)) # --> [1]
 print(sets_arrays([1,4,6,7], [1,3,5,8], False)) # --> [3, 4, 5, 6, 7, 8]
+
+
+#--------------- Otra forma de resolverlo ------------------
+def sets_array2(list1:list, list2:list, band:bool):
+
+    if len(list1) == 0 or len(list2) == 0:
+        return None
+    else:
+        list3 = []
+        if band:
+            for i in list1:
+                for j in list2:
+                    if i == j and i not in list3:
+                        list3 = list3 + [i]
+
+        else:
+            for i in list1:
+                if i not in list2:
+                    list3 = list3 + [i]
+        
+            for i in list2:
+                if i not in list1:
+                    list3 = list3 + [i]
+
+        return list3
+
+##### Test    
+print(sets_array2([1,2,3,5],[2,2,7], True)) # -->  [2]
+print(sets_array2([1,2,3,5],[2,2,7], False)) # --> [1, 3, 5, 7]
+print(sets_array2([],[2,2,7], False)) # --> None
