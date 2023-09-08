f6a303cec2cdd3cf051bd6a6944ba6f18901d2ab
jbernalg <jeinffersonbernal@gmail.com>
2023-09-08 08:16:25 -0500

    Reto 30.2

--- Retos/reto_30.py
+++ Retos/reto_30.py
@@ -46,3 +46,65 @@ print(bubbleSort([3,1,2,7,5,4,6], 'asc'))
 # [3,1,2,7,5,4,6], 'Asc' --> [1, 2, 3, 4, 5, 6, 7]
 # [3,1,2,7,5,4,6], 'Desc' --> [7, 6, 5, 4, 3, 2, 1]
 # [3,1,2,7,5,4,6], 'asc' --> Ingrese un tipo de orden correcto!
+
+
+
+##### Ordenamiento por Insercion
+
+def insertionSort(lista:list, orden:str):
+
+    # creamos una lista vacia que contendra los valores ordenados
+    lista_ord = []
+
+    # agregar el primer elemento a la lista ordenada
+    lista_ord = lista_ord + [lista[0]]
+
+    # orden ascendente
+    if orden == 'Asc':
+        
+        # recorremos los elementos de la lista y comparamos con elementos de lista ordenada
+        for i in range(1,len(lista)):
+    
+            for j in range(i - 1, -1, -1):
+                if lista_ord[j] <= lista[i]:
+                    lista_ord.insert(j + 1, lista[i])
+                    break
+            
+                else:
+                    if j == 0:
+                        lista_ord.insert(0, lista[i])
+            
+        return lista_ord
+    
+    # orden descendente
+    elif orden == 'Desc':
+
+        # recorremos los elementos de la lista y comparamos con elementos de lista ordenada
+        for i in range(1,len(lista)):
+    
+            for j in range(i - 1, -1, -1):
+                if lista_ord[j] >= lista[i]:
+                    lista_ord.insert(j + 1, lista[i])
+                    break
+            
+                else:
+                    if j == 0:
+                        lista_ord.insert(0, lista[i])
+            
+        return lista_ord
+    else:
+        return 'Ingrese un tipo de orden correcto!'
+
+##### Test
+print(insertionSort([3,1,2,2,5,4,6], 'Desc'))
+print(insertionSort([3,1,2,2,5,4,6], 'Asc'))
+print(insertionSort([3,1,7,2,5,4,6], 'Desc'))
+print(insertionSort([3,1,2,2,5,4,6], 'aesc'))
+
+# [3,1,2,2,5,4,6], 'Desc' --> [6, 5, 4, 3, 2, 2, 1]
+# [3,1,2,2,5,4,6], 'Asc' --> [1, 2, 2, 3, 4, 5, 6]
+# [3,1,7,2,5,4,6], 'Desc' --> [7, 6, 5, 4, 3, 2, 1]
+# [3,1,2,2,5,4,6], 'aesc' --> Ingrese un tipo de orden correcto!
+
+
+lista = [3,1,2,2,5,4,6]
