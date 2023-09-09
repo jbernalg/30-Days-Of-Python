5fc042f4830272d68d288b8fafe226a662401f97
jbernalg <jeinffersonbernal@gmail.com>
2023-09-09 09:18:30 -0500

    Repaso day 6.2

--- day_6_Tuples/practice6.py
+++ day_6_Tuples/practice6.py
@@ -56,39 +56,85 @@ print(type(tupla))
 '''
 
 #---------------Tuple Length--------------------
+fruits = ('banana', 'orange', 'mango', 'lemon', 2)
+'''
 print(len(fruits))
+5
+'''
 
 #---------------Accessing Tuples items-------------------
+##### Accede a las tuplas mediante los slicing
+fruits = ('banana', 'orange', 'mango', 'lemon', 'mora')
+'''
 print('first fruit:', fruits[0])
+first fruit: banana
+
 print('last fruits: ',fruits[len(fruits) - 1])
+last fruits:  mora
+
 print('first fruit: ', fruits[-5])
+first fruit:  banana
+
 print('last fruit: ', fruits[-1])
+last fruit:  mora
+'''
 
 #---------------Slicing tuples--------------------
-all_fruits = fruits[:] #all fruits
-print(all_fruits)
-orange_mango = fruits[1:3]
-print(orange_mango)
+fruits = ('banana', 'orange', 'mango', 'lemon', 'mora')
 
+#### Selecciona toda la tupla
+all_fruits = fruits[:] 
+'''
+print(all_fruits)
+('banana', 'orange', 'mango', 'lemon', 'mora')
+'''
+
+#### Selecciona una parte de la tupla
+orange_mango = fruits[1:3]
+'''
+print(orange_mango)
+('orange', 'mango')
+'''
+
+#### Otra forma de seleccionar partes de la tupla
+'''
 print('all fruits: ', fruits[-5:])
+all fruits:  ('banana', 'orange', 'mango', 'lemon', 'mora')
+
 print('orange mango: ', fruits[-4:-2])
+orange mango:  ('orange', 'mango')
+'''
 
 #---------------Changing tuples to list--------------------
+fruits = ('banana', 'orange', 'mango', 'lemon', 'mora')
 fruits = list(fruits)
+'''
 print(fruits)
+['banana', 'orange', 'mango', 'lemon', 'mora']
+'''
+
 fruits = tuple(fruits)
+'''
 print(fruits)
+('banana', 'orange', 'mango', 'lemon', 'mora')
+'''
 
 #----------------Checking an item in a tuple------------------
+'''
 print('orange' in fruits)
+True
+
 print('apple' in fruits)
+False
+'''
 
 #----------------Joining tuples----------------------
-fruits = ('banana', 'orange', 'mango', 'lemon', 2)
+fruits = ('banana', 'orange', 'mango', 'lemon', 'mora')
 vegetable = ('Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot')
 fruits_vegetable = fruits + vegetable
+'''
 print(fruits_vegetable)
-
+('banana', 'orange', 'mango', 'lemon', 'mora', 'Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot')
+'''
 #----------------Delete tuples--------------------
 del fruits
-
