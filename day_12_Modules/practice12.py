ff8109111522cfbc813dbeed869e3b4af9b91704
jbernalg <jeinffersonbernal@gmail.com>
2023-09-12 14:16:39 -0500

    day 12

--- day_12_Modules/practice12.py
+++ day_12_Modules/practice12.py
@@ -1,32 +1,92 @@
 #-------------------MODULES----------------------
-#A module is a file containing a set of codes or a set of functions which can be included to an application
-
+'''
+A module is a file containing a set of codes or a set of functions which 
+can be included to an application
+'''
 #-------------------Create a Module-------------------
-#To create a module we write our codes in a python script and we save it as a .py file.
+#### To create a module we write our codes in a python script and we save it as a .py file.
+#### Este tipo de llamado se conoce como names space
 import mymodule
+nombre_completo = mymodule.generate_full_name('Jose', 'Gimenez')
+'''
+print(nombre_completo)
+Jose Gimenez
+'''
 
-print(mymodule.generate_full_name('Jose', 'Gimenez'))
-
-#------------------Import Function from a Module-------------------
+#------------------Import Functions from a Module-------------------
 from mymodule import generate_full_name, sum_two_nums, gravity
 
-print(generate_full_name('Daniel', 'Pereira'))
-print(sum_two_nums(20, 18))
+nombre_completo = generate_full_name('Daniel', 'Diaz')
+suma_numeros = sum_two_nums(10,14)
+'''
+print(nombre_completo)
+print(suma_numeros)
+Daniel Diaz
+24
+'''
 
 mass = 100
 weight = mass * gravity
+'''
 print(weight)
+980.0000000000001
+'''
 
 #------------------Import Function from a Module and Renaming----------------
-#During importing we can rename the name of the module
+#### During importing we can rename the name of the module
 from mymodule import generate_full_name as fullname, sum_two_nums as total, gravity as g
-print(fullname('Manuel', 'Girot'))
-print(total(12, 20))
+nombre_completo = fullname('Diveana','Morales')
+result = total(12,6)
+'''
+print(nombre_completo)
+print(result)
+Diveana Morales
+18
+'''
 
 mass = 14
 weight = mass * g
+'''
 print(weight)
+137.20000000000002
+'''
 
+#### Importar todas las funciones de un modulo.
+#### Es una mala practica porque puede sobrecargar el programa
+from mymodule import  *
+nombre_completo = generate_full_name('Ligio', 'Vargas')
+result = sum_two_nums(12,13)
+'''
+print(nombre_completo)
+print(result)
+print(gravity)
+Ligio Vargas
+25
+9.8
+'''
+
+#----- Error al llamar una funcion que tiene el mismo nombre que una variable ------
+'''
+Al llamar una funcion que tiene el mismo nombre que una variable, se asigna
+el valor de variable y no la funcion por lo que es importante practicar metodologias
+de escritura para diferenciar ambos objetos 
+'''
+
+# ----- Metodos y propiedades de un modulo -----------
+
+##### Metodos del modulo importado
+'''
+print(dir(mymodule))
+['__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', 
+'__package__', '__spec__', 'generate_full_name', 'gravity', 'sum_two_nums']
+'''
+
+#### Nombre del metodo importado
+import mymodule as m_propio
+'''
+print(m_propio.__name__)
+mymodule
+'''
 
 #-----------------Import Built-in Modules------------------
 #math, datetime, os, sys, random, statistics, collections, json...
