7c96726046909e4891cd1ff4aeb20fa046148781
jbernalg <jeinffersonbernal@gmail.com>
2023-09-12 09:53:22 -0500

    repaso day 11.2

--- day_11_Functions/practice11.py
+++ day_11_Functions/practice11.py
@@ -107,35 +107,49 @@ def print_fullname(first_name, last_name):
     space = ' '
     full_name = first_name + space + last_name
     return full_name
-
+'''
 print(print_fullname(first_name='Daniel', last_name='Vergara'))
+Daniel Vergara
+'''
 
+#### el orden en que se pasan los valores no importan siempre que se declaren
 def add_two_numbers(num1, num2):
     total = num1 + num2
     return total
-
+'''
 print(add_two_numbers(num2 = 13, num1 = 2))
+15
+'''
 
 #---------------Function Returning a Value----------------------
 #Returning a string
 def print_name(first_name):
     return first_name
-print_name('Gerardo')
+'''
+print(print_name('Gerardo'))
+Gerardo
+'''
 
 #Returning a number:
 def calculate_age(current_year, birth_year):
     age = current_year - birth_year
-    return age;
+    return age
+'''
 print('Age: ', calculate_age(2022, 2003))
+Age: 19
+'''
 
 #Returning a boolean
 def is_even(n):
     if n % 2 == 0:
         return True
     return False
-
+'''
 print(is_even(10))
 print(is_even(21))
+True
+False
+'''
 
 #Returning a list:
 def find_even_numbers(n):
@@ -144,8 +158,10 @@ def find_even_numbers(n):
         if i % 2 == 0:
             even.append(i)
     return even
-
-print(find_even_numbers(87))
+'''
+print(find_even_numbers(10))
+[0, 2, 4, 6, 8, 10]
+'''
 
 #------------------- Desempaquetando valores de una funcion----------------------
 def value_oro_plata(p_petroleo):
@@ -180,23 +196,51 @@ Valor de la plata: 5.04
 def greetings(name = 'Peter'):
     message = name + ',Welcome to Python for Everyone!'
     return message
+'''
 print(greetings())
 print(greetings('Samuel'))
+Peter,Welcome to Python for Everyone!
+Samuel,Welcome to Python for Everyone!
+'''
 
-def calculate_age(birth_year, current_year = 2022):
+def calculate_age(birth_year, current_year = 2023):
     age = current_year - birth_year
     return age
+'''
 print('Age: ', calculate_age(1900))
+Age:  123
+'''
 
 #---------------Arbitrary number of arguments-----------------
+#### util cuando no hay una cantidad determinada de argumentos 
 def sum_all_nums(*nums):
     total = 0
     for num in nums:
         total += num
     return total
+'''
 print(sum_all_nums(2,3,7,11))
+23
+'''
+
+#### Podemos pasarle mas argumentos solo si se usa el * para el ultimo parametro
+def suma(nombre, *numeros):
+    return f'{nombre}, la suma de los numeros es: {sum(numeros)}'
+'''
+print(suma('Jose',1,3,5,7))
+Jose, la suma de los numeros es: 16
+'''
+
+#### Utilizar el operador * dentro de la funcion
+def suma_total(numeros):
+    return sum([*numeros])
+'''
+print(suma_total([1,3,5,7]))
+16
+'''
 
 #---------------Default and Arbitrary Number of parameters in Function------------------------
+#### Cantidad de argumentos indefinidos
 def generate_groups(team, *args):
     print(team)
     for i in args:
