52855dc2b5b10bf7888ce49198d7b943fb28bc13
jbernalg <jeinffersonbernal@gmail.com>
2023-09-14 10:19:57 -0500

    Reto 33.3

--- Retos/reto_33.py
+++ Retos/reto_33.py
@@ -40,6 +40,7 @@ def binary_search(lista, elemento):
 print(binary_search([1,3,5,7,9,12,15,20], 12))
 print(binary_search([1,3,5,7,9,12,15,20], 13))
 print(binary_search([1,3,5,7,9,12,15,20], 1))
+print('--------------------------------')
 
 # [1,3,5,7,9,12,15,20], 12 --> 5
 # [1,3,5,7,9,12,15,20], 13 --> -1
@@ -63,7 +64,47 @@ def sequential_search(lista, elemento):
 print(sequential_search([1,2,4,5,7,9,15,1,22], 15))
 print(sequential_search([1,2,4,5,7,9,15,1,22], 3))
 print(sequential_search([1,2,4,5,7,9,15,1,22], 22))
-
+print('--------------------------------')
 # [1,2,4,5,7,9,15,1,22], 15 --> 6
 # [1,2,4,5,7,9,15,1,22], 3 --> -1
 # [1,2,4,5,7,9,15,1,22], 8 --> 8
+
+##### Busqueda Exponencial
+
+def exponential_search(lista, elemento):
+
+    if lista[0] == elemento:
+        return 0
+    
+    n = len(lista)
+    i = 1
+    
+    # estimar un rango aproximado en el que se encuentra el elemento
+    while i < n and lista[i] <= elemento:
+        i *= 2
+
+    # aplicar la busqueda binaria en el rango aproximado
+    init = i//2
+    end = min(i, n - 1)
+
+    while init <= end:
+        
+        mid = (init + end)//2
+
+        if lista[mid] == elemento:
+            return mid
+        elif lista[mid] < elemento:
+            init = mid + 1
+        else:
+            end = mid - 1
+        
+    return -1
+    
+print(exponential_search([1,4,6,9,12,23,32,45,47,49], 45))
+print(exponential_search([1,4,6,9,12,23,32,45,47,49], 44))
+print(exponential_search([1,4,6,9,12,23,32,45,47,49], 49))
+print('--------------------------------')
+
+# [1,4,6,9,12,23,32,45,47,49], 45 --> 7
+# [1,4,6,9,12,23,32,45,47,49], 44 --> -1
+# [1,4,6,9,12,23,32,45,47,49], 49 -->
