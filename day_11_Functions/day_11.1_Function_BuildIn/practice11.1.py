a565cb4b50637284f35fb9dc92476d689a0e8948
jbernalg <jeinffersonbernal@gmail.com>
2023-09-14 14:07:48 -0500

    Repaso day 11

--- day_11_Functions/day_11.1_Function_BuildIn/practice11.1.py
+++ day_11_Functions/day_11.1_Function_BuildIn/practice11.1.py
@@ -69,3 +69,55 @@ suma = sum(numeros)
 print(suma)
 23
 '''
+
+#### range(): genera una secuencia de numeros
+rango = range(1,20)
+'''
+print(list(rango))
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+'''
+
+#### abs(): devuelve el valor absoluto de un numero
+absoluto = abs(-23)
+'''
+print(absoluto)
+23
+'''
+
+#### round(): redondea un numero a una cantidad especifica de decimales
+redondeo = round(123, 2)
+'''
+print(redondeo)
+123
+'''
+
+redondeo = round(123.456, 2)
+'''
+print(redondeo)
+123.46
+'''
+
+#### sorted(): ordena una secuencia
+tupla = (3,1,6,2,7,5)
+ordenado = sorted(tupla)
+'''
+print(ordenado)
+[1, 2, 3, 5, 6, 7]
+'''
+
+#### zip(): se utiliza para combinar elementos de dos o mas secuencias (lista, tuplas)
+# en tuplas emparejadas. Ambas secuencia deben tener el mismo size
+nombres = ['Marco', 'Daniel', 'Salomon']
+edades = [22,18,27]
+
+# combinamos los nombres y edades en tuplas emparejadas
+datos_personales = list(zip(nombres, edades))
+
+# recorres la lista
+'''
+for nombre, edad in datos_personales:
+    print(f'Nombre: {nombre}. Edad: {edad}')
+Nombre: Marco. Edad: 22
+Nombre: Daniel. Edad: 18
+Nombre: Salomon. Edad: 27
+'''
