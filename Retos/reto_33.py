c83fc6fc7c4348e271a5cadf0bf3ea8b3b01a187
jbernalg <jeinffersonbernal@gmail.com>
2023-09-14 09:51:18 -0500

    Reto 33.2

--- Retos/reto_33.py
+++ Retos/reto_33.py
@@ -36,7 +36,7 @@ def binary_search(lista, elemento):
         # si no encuentra el elemento, retornar -1
         return -1
     
-##### Test
+# Test
 print(binary_search([1,3,5,7,9,12,15,20], 12))
 print(binary_search([1,3,5,7,9,12,15,20], 13))
 print(binary_search([1,3,5,7,9,12,15,20], 1))
@@ -48,4 +48,22 @@ print(binary_search([1,3,5,7,9,12,15,20], 1))
 
 ######## Busqueda Lineal
 
-#def sequential_search(lista, elemento):
+def sequential_search(lista, elemento):
+    # recorre toda la lista
+    for i in enumerate(lista):
+        # si el valor de la lista coincide con el elemento, retornar posiciondel valor de lista
+        if i[1] == elemento:
+            return i[0]
+            # sale del loop
+            break
+        
+    return -1
+
+# Test
+print(sequential_search([1,2,4,5,7,9,15,1,22], 15))
+print(sequential_search([1,2,4,5,7,9,15,1,22], 3))
+print(sequential_search([1,2,4,5,7,9,15,1,22], 22))
+
+# [1,2,4,5,7,9,15,1,22], 15 --> 6
+# [1,2,4,5,7,9,15,1,22], 3 --> -1
+# [1,2,4,5,7,9,15,1,22], 8 --> 8
