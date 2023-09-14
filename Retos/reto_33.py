d20ebb245679ae52c3bb40d520a28a234e9e084b
jbernalg <jeinffersonbernal@gmail.com>
2023-09-14 09:45:23 -0500

    Reto 33

---
+++ Retos/reto_33.py
@@ -0,0 +1,51 @@
+'''
+Algoritmos de busqueda
+'''
+
+######### Busqueda binaria
+
+def binary_search(lista, elemento):
+
+    # definimos los valores inicial y final de la lista
+    init = 0
+    end = len(lista) - 1
+
+    # si el elemento buscado coincide con el primer valor de la lista, retornar posicion
+    if lista[init] == elemento:
+        return init
+    # si el elemento buscado coincide con el ultimo valor de la lista, retornar posicion
+    elif lista[end] == elemento:
+        return end
+    # buscar elemento dentro de la lista
+    else:
+
+        while init <= end:
+            # valor medio de la lista
+            mid = (init + end) // 2
+            # si el valor medio de la lista coincide, retornar valor medio
+            if lista[mid] == elemento:
+                return mid
+            # si el valor medio de la lista es menor, asignar al valor inicial la suma de
+            # valor medio mas 1
+            elif lista[mid] < elemento:
+                init = mid + 1
+            # si el valor medio de la lista es mayor, asignar al valor final la resta
+            # del valor medio menos 1
+            else:
+                end = mid - 1
+        # si no encuentra el elemento, retornar -1
+        return -1
+    
+##### Test
+print(binary_search([1,3,5,7,9,12,15,20], 12))
+print(binary_search([1,3,5,7,9,12,15,20], 13))
+print(binary_search([1,3,5,7,9,12,15,20], 1))
+
+# [1,3,5,7,9,12,15,20], 12 --> 5
+# [1,3,5,7,9,12,15,20], 13 --> -1
+# [1,3,5,7,9,12,15,20], 1 --> 0
+
+
+######## Busqueda Lineal
+
+#def sequential_search(lista, elemento):
